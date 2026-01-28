b7ec985b98d36b433457d5fca6c41c2dee7102d5
Korobitsyn73 <m.korobitsyn@gmail.com>
2026-01-29 01:13:52 +0500

    Домашка ООП с доработками

--- Lesson_03/Lesson_3_task_1.py
+++
@@ -1,9 +0,0 @@
-from Lesson_03.user import User
-
-
-class User:
-
-    my_user = User('Максим', 'Коробицын')
-    print(my_user.first_name)
-    print(my_user.last_name)
-    print(my_user.namefam())
