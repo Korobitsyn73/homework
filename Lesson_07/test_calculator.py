e13a245a86bc0c81fd1a764c7abe03cb40bf66af
Korobitsyn73 <m.korobitsyn@gmail.com>
2026-03-15 18:48:19 +0500

    Домашка урок 8

--- Lesson_07/test_calculator.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from pages_object.Calculator import Calculator
-
-
-def test_slow_calc():
-    browser = webdriver.Chrome()
-    calculator = Calculator(browser)
-    calculator.calc_start()
